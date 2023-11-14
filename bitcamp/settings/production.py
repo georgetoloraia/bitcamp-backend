a6f602e362b4d22522f98346b599e8d82ab681c2
NickKipshidze <kipshidze.nick@gmail.com>
2023-11-14 19:45:22 +0400

    Fix settings

--- bitcamp/settings/production.py
+++ bitcamp/settings/production.py
@@ -2,3 +2,14 @@ import os
 from .base import *
 
 SECRET_KEY = os.environ["SECRET_KEY"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT")
+    }
+}
