c3a6cb55d92dc2faa82c8617bfc35da440c56a08
NickKipshidze <kipshidze.nick@gmail.com>
2023-11-16 13:53:33 +0400

    Cleanup code & write comments

--- bitbot/bot.py
+++ bitbot/bot.py
@@ -1,11 +1,14 @@
 import discord, copy, os
 
+# Each "service channel" representation
 channels = {}
 
+# Custom types for questions
 class InputType:
     Text = 1
     Choice = 2
 
+# Each "service channel" will be represented in the memmory in this format 
 CHANNEL_TEMPLATE = {
     "user_id": None,
     "question": 0,
@@ -14,91 +17,41 @@ CHANNEL_TEMPLATE = {
     "confirmed": False
 }
 
-ANSWERS = [
-    {
-        "label": "სახელი და გვარი",
-        "value": None
-    },
-    {
-        "label": "პირადი ნომერი",
-        "value": None
-    },
-    {
-        "label": "თელეფონის ნომერი",
-        "value": None
-    },
-    {
-        "label": "ელ.ფოსტის მისამართი",
-        "value": None
-    },
-    {
-        "label": "შერჩეული პროგრამის დასახელება",
-        "value": None
-    },
-]
-
-QUESTIONS = [
-    {
-        "title": "თქვენი სახელი და გვარი?",
-        "description": None,
-        "input_type": InputType.Text,
-        "choices": None
-    },
-    {
-        "title": "თქვენი პირადი ნომერი?",
-        "description": None,
-        "input_type": InputType.Text,
-        "choices": None
-    },
-    {
-        "title": "თქვენი ტელეფონის ნომერი?",
-        "description": None,
-        "input_type": InputType.Text,
-        "choices": None
-    },
-    {
-        "title": "თქვენი ელ.ფოსტის მისამართი?",
-        "description": None,
-        "input_type": InputType.Text,
-        "choices": None
-    },
-    {
-        "title": "შერჩეული პროგრამის დასახელება?",
-        "description": "აირჩიეთ შერჩეული პროგრამის დასახელება",
-        "input_type": InputType.Choice,
-        "choices": [
-            "front-end-react",
-            "front-end-vue",
-            "back-end-python",
-            "back-end-node",
-            "pro",
-            "kids"
-        ]
-    }
-]
-
 class Client(discord.Client):
+    def __init__(self, *, intents, questions, answers, **options):
+        super().__init__(intents=intents, **options)
+        
+        self.questions = questions
+        self.answers = answers
+    
+    # When the Discord bot is ready
     async def on_ready(self):
         print(f"[BitBot] Logged on as {self.user}!")
         
+    # Proceed to the next question
     async def next_question(self, channel):
         channel_data = channels[channel.id]
         
         if channel_data["confirmed"]:
             await channel.send("დამთავრდა რეგისტრაცია")
+            # Here we can save the registration data in our databse
             print("დამთავრდა რეგისტრაცია, პასუხები:", channel_data["answers"])
             return
         
         try:
-            question = QUESTIONS[channel_data["question"]]
+            question = self.questions[channel_data["question"]]
         except IndexError:
+            # If there was an IndexError, that means the last question
+            # was answered, so we prompt for confirmation
             channel_data["done"] = True
             await self.prompt_confirm(channel)
             return
         
+        # If its a text question
         if question["input_type"] == InputType.Text:
             await channel.send(question["title"])
         
+        # If its a choice question (buttons)
         if question["input_type"] == InputType.Choice:
             question_embed = discord.Embed(
                 title=question["title"],
@@ -114,6 +67,7 @@ class Client(discord.Client):
             
             await channel.send(embed=question_embed, view=row)
     
+    # Prompt for confirmation of answers
     async def prompt_confirm(self, channel):
         channel_data = channels[channel.id]
         answers = channel_data["answers"]
@@ -135,6 +89,9 @@ class Client(discord.Client):
             color=0x7289DA
         )
         
+        # This buttons have special keywords in their custom_id
+        # Avoid using exact same keyword custom_id in actual choice questions
+        
         row = discord.ui.View()
         row.add_item(
             discord.ui.Button(style=discord.ButtonStyle.green, label="დადასტურება", custom_id="CONFIRM")
@@ -146,6 +103,7 @@ class Client(discord.Client):
         
         await channel.send(embed=confirm_embed, view=row)
 
+    # Listen for new users joining the server
     async def on_member_join(self, member):
         channel = await member.guild.create_text_channel(f"{member.name}")
         
@@ -162,23 +120,32 @@ class Client(discord.Client):
         
         channels[channel.id] = copy.deepcopy(CHANNEL_TEMPLATE)
         channels[channel.id]["user_id"] = member.id
-        channels[channel.id]["answers"] = copy.deepcopy(ANSWERS)
+        channels[channel.id]["answers"] = copy.deepcopy(self.answers)
         
         await self.next_question(channel)
 
+    # Listen for interactions (button click, checkboxes, etc)
     async def on_interaction(self, interaction):
+        # If its a button click
         if interaction.data["component_type"] == 2:
+            # Get the data of our "service channel"
             channel_data = channels[interaction.channel.id]
             
+            # Check if the user who sent the interaction is the one we are serving in this channel
+            # Check if the registration is over or not
             if interaction.user.id == channel_data["user_id"] and channel_data["confirmed"] == False:
+                # Remove the buttons so that the user cannot click them again
                 await interaction.message.edit(view=None)
-                    
+                
+                # Get the button ID
                 custom_id = interaction.data["custom_id"]
                 
+                # If on confirmation stage, we check for CHANGE and CONFIRM buttons
                 if channel_data["done"] and custom_id.split("_")[0] == "CHANGE":
                     channels[interaction.channel.id]["question"] = int(custom_id.split("_")[1])
                 elif channel_data["done"] and custom_id == "CONFIRM":
                     channels[interaction.channel.id]["confirmed"] = True
+                # If not on confirmation stage, we accept any buttons
                 else:
                     question = channel_data["question"]
                     
@@ -189,8 +156,10 @@ class Client(discord.Client):
                         f"მოინიშნა **{custom_id}**."
                     )
                     
+                # We proceed to the next question
                 await self.next_question(interaction.channel)
 
+    # Listen for messages on entire server
     async def on_message(self, message):
         # DEBUG #
         if message.channel.id == 1173695205266948136 and message.content == "test me" and message.author.id != 1173667963249897582:
@@ -198,20 +167,27 @@ class Client(discord.Client):
             await self.on_member_join(message.author)
         # DEBUG #
         
+        # Check if there was a message sent in one of the "service channels"
         if message.channel.id in channels:
+            # Get the data of our "service channel"
             channel_data = channels[message.channel.id]
             
+            # If the message author is the user we are serving
             if message.author.id == channel_data["user_id"] and channel_data["confirmed"] == False:
-                question = QUESTIONS[channel_data["question"]]
+                question = self.questions[channel_data["question"]]
                 
+                # If we are waiting for a text input
                 if question["input_type"] == InputType.Text:
                     channels[message.channel.id]["answers"][channel_data["question"]]["value"] = message.content
                     
+                    # If on confirmation stage
                     if channel_data["done"]:
-                        channels[message.channel.id]["question"] = len(QUESTIONS)
+                        # We return to the last question
+                        channels[message.channel.id]["question"] = len(self.questions)
                     else:
                         channels[message.channel.id]["question"] += 1
                     
+                    # We proceed to the next question
                     await self.next_question(message.channel)
 
 def main():
@@ -219,7 +195,77 @@ def main():
     intents.message_content = True
     intents.members = True
 
-    client = Client(intents=intents)
+    client = Client(
+        intents=intents,
+        
+        # Questions list for testing
+        # We will load this from the database
+        questions=[
+            {
+                "title": "თქვენი სახელი და გვარი?",
+                "description": None,
+                "input_type": InputType.Text,
+                "choices": None
+            },
+            {
+                "title": "თქვენი პირადი ნომერი?",
+                "description": None,
+                "input_type": InputType.Text,
+                "choices": None
+            },
+            {
+                "title": "თქვენი ტელეფონის ნომერი?",
+                "description": None,
+                "input_type": InputType.Text,
+                "choices": None
+            },
+            {
+                "title": "თქვენი ელ.ფოსტის მისამართი?",
+                "description": None,
+                "input_type": InputType.Text,
+                "choices": None
+            },
+            {
+                "title": "შერჩეული პროგრამის დასახელება?",
+                "description": "აირჩიეთ შერჩეული პროგრამის დასახელება",
+                "input_type": InputType.Choice,
+                "choices": [
+                    "front-end-react",
+                    "front-end-vue",
+                    "back-end-python",
+                    "back-end-node",
+                    "pro",
+                    "kids"
+                ]
+            }
+        ],
+        
+        # Answers list for storing answers with their labels
+        # We will load this from the database
+        answers=[
+            {
+                "label": "სახელი და გვარი",
+                "value": None
+            },
+            {
+                "label": "პირადი ნომერი",
+                "value": None
+            },
+            {
+                "label": "თელეფონის ნომერი",
+                "value": None
+            },
+            {
+                "label": "ელ.ფოსტის მისამართი",
+                "value": None
+            },
+            {
+                "label": "შერჩეული პროგრამის დასახელება",
+                "value": None
+            },
+        ]
+    )
+    
     client.run(os.environ["DISCORD_BOT_TOKEN"])
 
 if __name__ == "__main__":
