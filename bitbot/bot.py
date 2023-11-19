708cdc24b41269992b9d4660c04f06b4ef2f4a3d
NickKipshidze <kipshidze.nick@gmail.com>
2023-11-19 14:16:16 +0400

    Import configuration from .asks

--- bitbot/bot.py
+++ bitbot/bot.py
@@ -1,13 +1,8 @@
 import discord, copy, os
+from .asks import InputType, QUESTIONS
 
 # Each "service channel" representation
 channels = {}
-
-# Custom types for questions
-class InputType:
-    Text = 0x1
-    Choice = 0x2
-    Attachment = 0x3
 
 # Each "service channel" will be represented in the memmory in this format 
 CHANNEL_TEMPLATE = {
@@ -219,72 +214,7 @@ def main():
         
         # Questions list for testing
         # We will load this from the database
-        questions=[
-            {
-                "label": "სახელი და გვარი",
-                "title": "თქვენი სახელი და გვარი?",
-                "description": None,
-                "input_type": InputType.Text,
-                "choices": None
-            },
-            {
-                "label": "პირადი ნომერი",
-                "title": "თქვენი პირადი ნომერი?",
-                "description": None,
-                "input_type": InputType.Text,
-                "choices": None
-            },
-            {
-                "label": "თელეფონის ნომერი",
-                "title": "თქვენი ტელეფონის ნომერი?",
-                "description": None,
-                "input_type": InputType.Text,
-                "choices": None
-            },
-            {
-                "label": "ელ.ფოსტის მისამართი",
-                "title": "თქვენი ელ.ფოსტის მისამართი?",
-                "description": None,
-                "input_type": InputType.Text,
-                "choices": None
-            },
-            {
-                "label": "შერჩეული პროგრამა",
-                "title": "შერჩეული პროგრამის დასახელება?",
-                "description": "აირჩიეთ შერჩეული პროგრამის დასახელება",
-                "input_type": InputType.Choice,
-                "choices": [
-                    "front-end-react",
-                    "front-end-vue",
-                    "back-end-python",
-                    "back-end-node",
-                    "pro",
-                    "kids"
-                ]
-            },
-            {
-                "label": "შერჩეული პაკეტი",
-                
-                "if_answer": {
-                    "label": "შერჩეული პროგრამა",
-                    "is_value": ["front-end-react", "back-end-python", "back-end-node"]
-                },
-                
-                "title": "აირჩიეთ პაკეტი",
-                "description": "აირჩიეთ შერჩეული პროგრამის პაკეტი",
-                "input_type": InputType.Choice,
-                "choices": [
-                    "საერთო სამენტორო მომსახურება",
-                    "პირადი მენტორის აყვანა"
-                ]
-            },
-            {
-                "label": "ქვითარი",
-                "title": "ატვირთეთ ქვითრის ფაილი",
-                "description": None,
-                "input_type": InputType.Attachment
-            }
-        ]
+        questions=QUESTIONS
     )
     
     client.run(os.environ["DISCORD_BOT_TOKEN"])
