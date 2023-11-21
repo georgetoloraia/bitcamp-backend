51d57512ee65e24fbd736637c95c793304433a58
NickKipshidze <kipshidze.nick@gmail.com>
2023-11-21 19:12:46 +0400

    Boilerplate code for content models

---
+++ content/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Program(models.Model):
+    NotImplementedError
+
+class Service(models.Model):
+    NotImplementedError
+
+class Mentor(models.Model):
+    NotImplementedError
