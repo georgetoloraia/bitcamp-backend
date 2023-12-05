d823f4ea3f4a69090c0f44a896f85cdb52886001
NickKipshidze <kipshidze.nick@gmail.com>
2023-12-05 18:51:50 +0400

    Add dynamically generated questions

--- bitbot/asks.py
+++ bitbot/asks.py
@@ -2,12 +2,15 @@
 # Discord bot's conversation configuration
 # Configure what the Discord bot asks the user
 
+from content import models
+
 # Custom types for questions
 class InputType:
     Text = 0x1
     Choice = 0x2
     Attachment = 0x3
 
+# Todo: Write documentation for this configuration
 QUESTIONS = [
     {
         "label": "სახელი და გვარი",
@@ -41,44 +44,51 @@ QUESTIONS = [
             "თუ გაქვთ ფასდაკლების/Promo კოდი, გთხოვთ ამ ჩათში მოგვწეროთ და შეგეძლებათ ფასდაკლებით სარგებლობა.",
         "input_type": InputType.Choice,
         "choices": [
-            "front-end-react",
-            # "front-end-vue",
-            "back-end-python",
-            "back-end-node",
-            "pro",
-            # "kids"
+            program.title for program in models.Program.objects.all()
         ]
-    },
-    {
-        "label": "შერჩეული პაკეტი",
-        
-        "if_answer": {
-            "label": "შერჩეული პროგრამა",
-            "is_value": ["front-end-react", "back-end-python", "back-end-node"]
-        },
-        
-        "title": "აირჩიეთ პაკეტი",
-        "description": "აირჩიეთ შერჩეული პროგრამის პაკეტი",
-        "input_type": InputType.Choice,
-        "choices": [
-            "საერთო სამენტორო მომსახურება",
-            "პირადი მენტორის აყვანა"
-        ]
-    },
-    {
-        "label": "",
-        "title": "სწავლის საფასურის ჩარიცხვა",
-        "description":
-            "თქვენს მიერ შერჩეული მომსახურებისა და ტარიფის მიხედვით ჩარიცხეთ საფასური ქვემოთ მოცემული მონაცემების გამოყენებით.\n\n"
-            "ანგარიში: `GE75TB7003815365100012`\n"
-            "მიმღები: `ოთარ ზაკალაშვილი`\n"
-            "დანიშნულება: თქვენი სახელი და გვარი",
-        "input_type": None
-    },
-    {
-        "label": "ქვითარი",
-        "title": "ატვირთეთ ქვითრის ფაილი",
-        "description": None,
-        "input_type": InputType.Attachment
     }
 ]
+
+# This is where it gets complicated
+for program in models.Program.objects.all():
+    # Here we basically generate a service choice question for each program
+    QUESTIONS.append(
+        {
+            "label": "შერჩეული პაკეტი",
+            
+            "if_answer": {
+                "label": "შერჩეული პროგრამა",
+                "is_value": [program.title]
+            },
+            
+            "title": "აირჩიეთ პაკეტი",
+            "description": "აირჩიეთ შერჩეული პროგრამის პაკეტი",
+            "input_type": InputType.Choice,
+            "choices": [
+                service.title for service in program.services.all()
+            ]
+        }
+    )
+    
+    # And here we check if any service has mentors
+    for service in program.services.all():
+        # If the service has more than 0 mentors then we prompt the user
+        # to choose one
+        if len(service.mentors.all()) >= 1:
+            QUESTIONS.append(
+                {
+                    "label": "აყვანილი მენტორი",
+                    
+                    "if_answer": {
+                        "label": "შერჩეული პაკეტი",
+                        "is_value": [service.title]
+                    },
+                    
+                    "title": "აიყვანეთ მენტორი",
+                    "description": "აიყვანეთ პირადი მენტორი",
+                    "input_type": InputType.Choice,
+                    "choices": [
+                        mentor.fullname for mentor in service.mentors.all()
+                    ]
+                }
+            )
