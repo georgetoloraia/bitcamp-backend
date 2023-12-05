9fa64897a70dcdb86665585fb383710f133fffb8
NickKipshidze <kipshidze.nick@gmail.com>
2023-12-05 18:52:21 +0400

    Define fields for Mentor, Service and Program

--- content/models.py
+++ content/models.py
@@ -1,12 +1,52 @@
 from django.db import models
 
 # Create your models here.
-
-class Program(models.Model):
-    NotImplementedError
+    
+class Mentor(models.Model):
+    fullname = models.CharField(
+        max_length=32,
+        blank=False,
+        null=False
+    )
+    
+    biography = models.CharField(
+        verbose_name="Information about the mentor",
+        max_length=256,
+        blank=True,
+        null=True
+    )
 
 class Service(models.Model):
-    NotImplementedError
+    title = models.CharField(
+        verbose_name="Service title",
+        max_length=32,
+        blank=False,
+        null=False
+    )
+    
+    price = models.DecimalField(
+        verbose_name="Price of service",
+        max_digits=6,
+        decimal_places=2,
+        blank=False,
+        null=False
+    )
+    
+    mentors = models.ManyToManyField(
+        to=Mentor,
+        blank=True
+    )
 
-class Mentor(models.Model):
-    NotImplementedError
+class Program(models.Model):
+    title = models.CharField(
+        verbose_name="Program title",
+        max_length=32,
+        blank=False,
+        null=False
+    )
+    
+    services = models.ManyToManyField(
+        verbose_name="Program services",
+        to=Service,
+        blank=False
+    )
