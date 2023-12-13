e35e433ab910e2f1015946e481370c8f33b32f5d
NickKipshidze <kipshidze.nick@gmail.com>
2023-12-13 04:59:00 +0400

    Include accounts.urls

--- bitcamp/urls.py
+++ bitcamp/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("auth/", include("accounts.urls"), name="auth"),
     path("api/schema", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs", SpectacularSwaggerView.as_view(url_name="schema"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
