00d22428a4a699e261fd4ce8b141b2fdbc547b22
NickKipshidze <kipshidze.nick@gmail.com>
2024-01-03 12:05:01 +0400

    Add Enrollment serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,5 +6,19 @@ class BitCampUserSerializer(serializers.ModelSerializer):
     class Meta(object):
         model = models.BitCampUser
         fields = [
-            "id", "username", "email", "phone_number", "password"
+            "id", "first_name", "last_name", "phone_number", "email", "username", "password"
         ]
+
+class EnrollmentSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = models.Enrollment
+        fields = [
+            "id", "enrollment_date", "enrollment_cancel_date", "user_id", "service_id", "program_id", "mentor_id", "status"
+        ]
+    
+    def create(self, validated_data):
+        user = self.context.get("user")
+        if user:
+            validated_data["user_id"] = user
+
+        return super().create(validated_data)
