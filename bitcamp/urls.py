2db6f0f441dc2bd651307551019d437fe0913988
NickKipshidze <kipshidze.nick@gmail.com>
2024-01-03 12:06:47 +0400

    Change accounts.url route

--- bitcamp/urls.py
+++ bitcamp/urls.py
@@ -7,7 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("accounts.urls"), name="auth"),
+    path("", include("accounts.urls")),
     path("api/schema", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs", SpectacularSwaggerView.as_view(url_name="schema"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
