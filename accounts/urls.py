f9f519cba7f1fe32eaae580a6677b84a1bccc43b
NickKipshidze <kipshidze.nick@gmail.com>
2024-01-03 12:05:15 +0400

    Add enroll route

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("signup", views.SignupUser.as_view(), name="signup"),
-    path("login", views.LoginUser.as_view(), name="login"),
-    path("profile", views.CurrentUser.as_view(), name="user")
+    path("auth/signup", views.SignupUser.as_view(), name="signup"),
+    path("auth/login", views.LoginUser.as_view(), name="login"),
+    path("auth/profile", views.CurrentUser.as_view(), name="user"),
+    path("enroll", views.Enroll.as_view(), name="enrollment")
 ]
